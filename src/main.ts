e691a94766e00ca44118d94580c1f99e89469aa9
Anthony Rodriguez <107444528+Piratemonkey941@users.noreply.github.com>
2023-08-14 08:10:35 -0500

    starting cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css';
+import 'quasar/src/css/index.sass';
+import './style.css';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+const myApp = createApp(App);
+createApp(App).mount('#app');
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+
+myApp.mount('#app');
