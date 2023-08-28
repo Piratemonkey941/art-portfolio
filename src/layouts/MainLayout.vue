0850a30e0d94d97e8ca7c3fbd1133df84efc0bbf
Anthony Rodriguez <107444528+Piratemonkey941@users.noreply.github.com>
2023-08-28 08:09:45 -0500

    add chancges.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="mainHeader">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Pirate Monkey Painting </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <!-- <p style="color: wheat">test</p> -->
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style lang="scss" scoped>
+.mainHeader {
+  background-color: rgba(4, 0, 24, 0.747);
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component title="Example component" active></example-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { ExampleComponent },
+  setup() {},
+});
+//not sure what this page is for
+</script>
